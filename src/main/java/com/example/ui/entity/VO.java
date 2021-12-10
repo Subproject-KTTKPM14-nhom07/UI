8ef4d3c28c052cf47d03ff549e407229eec23a48
“Hao-Prime” <anhhaoprime159@gmail.com>
2021-12-10 16:41:28 +0700

    cm lần đầu

---
+++ src/main/java/com/example/ui/entity/VO.java
@@ -0,0 +1,33 @@
+package com.example.ui.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class VO {
+    private User user;
+    private Post post;
+//    public VO() {
+//		// TODO Auto-generated constructor stub
+//	}
+//	public VO(User user, Post post) {
+//		super();
+//		this.user = user;
+//		this.post = post;
+//	}
+//	public User getUser() {
+//		return user;
+//	}
+//	public void setUser(User user) {
+//		this.user = user;
+//	}
+//	public Post getPost() {
+//		return post;
+//	}
+//	public void setPost(Post post) {
+//		this.post = post;
+//	}
+}
